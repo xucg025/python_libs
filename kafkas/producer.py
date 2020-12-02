9b062add754f1745fdce3064b97f75b417fb8bd2
xuchenggang <Abc123456>
2020-12-02 18:15:35 +0800

    add

---
+++ kafkas/producer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# @author: Spark
+# @file: main.py
+# @ide: PyCharm
+# @time: 2020-12-02 16:57:59
+
+from kafka import KafkaProducer
+import json
+
+# producer = KafkaProducer(bootstrap_servers=['192.168.174.30:9092'])
+# future = producer.send('my_topic', key=b'my_key', value=b'my_value', partition= 0)
+# result = future.get(timeout=10)
+# print(result)
+
+producer = KafkaProducer(bootstrap_servers='192.168.174.30:9092')
+
+msg_dict = {
+    "sleep_time": 10,
+    "db_config": {
+        "database": "test_1",
+        "host": "xxxx",
+        "user": "root",
+        "password": "root"
+    },
+    "table": "msg",
+    "msg": "Hello World"
+}
+msg = json.dumps(msg_dict)
+for _ in range(100):
+    producer.send('xcg', msg.encode(), partition=1)
+producer.close()
